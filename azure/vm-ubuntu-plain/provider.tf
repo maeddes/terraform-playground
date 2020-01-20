433f50f2643a12032022e6077637dcab1271073a
Matthias <maeddes@gmail.com>
2020-01-20 22:07:26 +0100

    removed IDs, exported to env vars

--- azure/vm-ubuntu-plain/provider.tf
+++ azure/vm-ubuntu-plain/provider.tf
@@ -1,4 +1,4 @@
 provider "azurerm" {
-    subscription_id = ${ARM_SUBSCRIPTION_ID}
-    tenant_id       = ${ARM_TENANT_ID}
+    #subscription_id = ARM_SUBSCRIPTION_ID
+    #tenant_id       = ARM_TENANT_ID
 }
