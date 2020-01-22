7bf76265374f985df0332c2be263664ecfaf40a8
Matthias <maeddes@gmail.com>
2020-01-22 21:29:03 +0100

    patches

--- azure/kubernetes-cluster/kubernetes.tf
+++ azure/kubernetes-cluster/kubernetes.tf
@@ -1,21 +1,24 @@
 variable "name" {}
-variable "sp_client_id" {}
-variable "sp_client_secret" {}
+
+provider "azurerm" {
+    #subscription_id = ARM_SUBSCRIPTION_ID
+    #tenant_id       = ARM_TENANT_ID
+}
 
 resource "azurerm_resource_group" "this" {
-  name     = "${var.name}-aks"
+  name     = "${var.name}-resource-group"
   location = "westeurope"
 }
 
 resource "azurerm_virtual_network" "this" {
-  name                = "${var.name}"
+  name                = "${var.name}-virtual-net"
   location            = "westeurope"
   resource_group_name = azurerm_resource_group.this.name
   address_space       = ["10.0.0.0/8"]
 }
 
 resource "azurerm_subnet" "this" {
-  name                 = "${var.name}"
+  name                 = "${var.name}-subnet"
   resource_group_name  = azurerm_resource_group.this.name
   virtual_network_name = azurerm_virtual_network.this.name
   address_prefix       = "10.10.0.0/20"
@@ -39,8 +42,4 @@ resource "azurerm_kubernetes_cluster" "this" {
     vnet_subnet_id  = azurerm_subnet.this.id
   }
  
-   service_principal {
-    client_id     = var.sp_client_id
-    client_secret = var.sp_client_secret
-  }
 }
