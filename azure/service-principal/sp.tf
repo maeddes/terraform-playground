b7d655a3e84b3540445db3d6ff44d2df81d1b5ac
Matthias <maeddes@gmail.com>
2020-01-23 11:29:05 +0100

    removed K8s part

--- azure/service-principal/sp.tf
+++ azure/service-principal/sp.tf
@@ -2,13 +2,34 @@ variable "name" {}
 variable "sp_client_id" {}
 variable "sp_client_secret" {}
 
-resource "azurerm_resource_group" "this" {
-  name     = "${var.name}-aks"
-  location = "westeurope"
+resource "azuread_service_principal" "service_principal" {
+  application_id = "${azuread_application.application.application_id}"
 }
- 
-   service_principal {
-    client_id     = var.sp_client_id
-    client_secret = var.sp_client_secret
+
+resource "random_string" "password" {
+  length  = 32
+  upper   = true
+  lower   = true
+  number  = true
+  special = true
+}
+
+resource "azuread_service_principal_password" "service_principal_password" {
+  service_principal_id = "${azuread_service_principal.service_principal.id}"
+  value                = "${random_string.password.result}"
+  end_date_relative    = "8760h"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "${var.name}"
+  location            = "${var.location}"
+  resource_group_name = "${var.resource_group_name}"
+  dns_prefix          = "${var.dns_prefix}"
+  kubernetes_version  = "${var.kubernetes_version}"
+
+  service_principal {
+    client_id     = "${azuread_service_principal.service_principal.id}"
+    client_secret = "${random_string.password.result}"
   }
+  
 }
