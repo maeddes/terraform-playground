1980f6903ae9dee08ccc2d12d90d12a74b3fe7d6
Matthias <maeddes@gmail.com>
2020-01-28 16:29:32 +0100

    added variable

--- aws/first/main.tf
+++ aws/first/main.tf
@@ -1,14 +1,25 @@
+variable "profile" {
+}
+
 provider "aws" {
   region = "eu-central-1"
-  #profile = "demo"
+  profile = var.profile
 }
+
 data "aws_caller_identity" "current" {}
+
+output "profile" {
+  value = var.profile != "" ? var.profile : "default"
+}
+
 output "account_id" {
   value = "${data.aws_caller_identity.current.account_id}"
 }
+
 output "caller_arn" {
   value = "${data.aws_caller_identity.current.arn}"
 }
+
 output "caller_user" {
   value = "${data.aws_caller_identity.current.user_id}"
 }
